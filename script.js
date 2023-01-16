88125026939b0ef058ccb05770926a64c1b92843
unknown <manishtomar998@gmail.com>
2023-01-17 00:21:23 +0530

    check

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your code here
+let evaluatedText=document.getElementById("evaluatedText");
+let letterCount=document.getElementById("letterCount");
+
+evaluatedText.addEventListener("input",calculateLength);
+
+function calculateLength() {
+    let word=evaluatedText.value;
+    let wordLength=word.length;
+    letterCount.innerHTML=wordLength;
+}
